6e078b88643259346cf0dd49f4ab28024cd42664
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-10-17 15:46:08 +0100

    worked on auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import React, {useState} from 'react';
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from './pages/Home/Home';
 import MyEvents from './pages/MyEvents/MyEvents';
 import ExploreEvents from './pages/ExploreEvents/ExploreEvents';
 import Navbar from './components/Navbar/Navbar';
 import CreateEvents from './pages/CreateEvents/CreateEvents';
+import Signup from './pages/Auth/Signup';
+import Login from './pages/Auth/Login';
 import image from '../src/assets/home-1.png'
+import AuthNav from './components/Navbar/AuthNav';
 // import './App.css'
 
 const App = () => {
+  const location = useLocation();
+  const [isLoggedin, setIsLoggedin] = useState(true);
+
+  const handleLogout = () => {
+    setIsLoggedin(false);
+  }
+
+  const isAuthPage = location.pathname === '/login' || location.pathname === '/signup';
+  const authNav = <AuthNav />;
+
+
   return (
     <>
-    <Navbar />
+    {!isAuthPage ? <Navbar isLoggedin={isLoggedin} onLogout={handleLogout} /> : authNav }
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/myevents" element={<MyEvents />} />
         <Route path="/explore" element={<ExploreEvents />} />
         <Route path="/createevents" element={<CreateEvents />} />
---
+++ src/components/Navbar/AuthNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AuthNav() {
+  return (
+    <nav className='px-8 pt-10 pb-4 items-center'>
+        <div className='logo'>
+        <p>EventHive</p>
+      </div>
+    </nav>
+  )
+}
