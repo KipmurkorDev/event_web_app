de1a27f3fd2c892bfcbe77a8b2a1b5cb5eef7b83
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-10-29 05:57:31 +0100

    got data for my-events

--- src/pages/ExploreEvents/ExploreEvents.jsx
+++ src/pages/ExploreEvents/ExploreEvents.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
+import { FaLocationArrow } from 'react-icons/fa';
 
 const auth_token = import.meta.env.VITE_AUTH_TOKEN;
 const baseUrl = import.meta.env.VITE_APP_API_URL;
@@ -10,26 +11,56 @@ function ExploreEvents() {
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await axios.get(`${baseUrl}/events/my-events`, {
+        const response = await axios.get(`${baseUrl}/events`, {
           headers: {
             Authorization: `Bearer ${auth_token}`
           },
-        })
-        console.log(response)
-        // set event data in the state
-        setEvents(response.data)
+        });
+        console.log(response);
+
+        if (Array.isArray(response.data.data)) {
+          // Ensure that response.data is an array
+          setEvents(response.data.data);
+        } else {
+          console.error('Response data is not an array:', response.data);
+        }
       } catch (error) {
-        console.error('Error fetching events:', error)
+        console.error('Error fetching events:', error);
       }
     };
+
     // Call the async function when the component mounts
     fetchEvents();
-  },[])
+  }, []);
+
 
 
   return (
     <div>
       <h2>Explore Events</h2>
+      <div>
+        {events.map((event) => {
+          return (
+            <div className="card flex justify-between">
+              <img src={event.image} alt={event.category} />
+              <div>
+              <p>{event.eventName}</p>
+              <h2>{event.category}</h2>
+              <div className="flex">
+                <div>
+                <div>
+                <FaLocationArrow />
+                <p>{event.dateTime}</p>
+              </div>
+                </div>
+              </div>
+
+
+              </div>
+            </div>
+          )
+        })}
+      </div>
 
     </div>
   )
--- src/pages/MyEvents/MyEvents.jsx
+++ src/pages/MyEvents/MyEvents.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-const MyEvents = () => {
+const auth_token = import.meta.env.VITE_AUTH_TOKEN;
+const baseUrl = import.meta.env.VITE_APP_API_URL;
+
+function MyEvents() {
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/events/my-events`, {
+          headers: {
+            Authorization: `Bearer ${auth_token}`
+          },
+        })
+        console.log(response)
+        // set event data in the state
+        setEvents(response.data)
+      } catch (error) {
+        console.error('Error fetching events:', error)
+      }
+    };
+    // Call the async function when the component mounts
+    fetchEvents();
+  },[])
+
+
   return (
-    <div>MyEvents</div>
+    <div>
+      <h2>My Events</h2>
+
+    </div>
   )
 }
 
